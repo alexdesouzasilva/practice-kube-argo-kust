4ffecb4c173e4f78c3d257f868ee4c32cfc64add
Alex <alexdesouzasilva94@gmail.com>
2024-09-12 19:01:09 -0300

    alterando camada resources

--- src/main/java/br/com/devsenior/practice_kube_argo_kust/resources/PracticeKubeArgoKust.java
+++ src/main/java/br/com/devsenior/practice_kube_argo_kust/resources/PracticeKubeArgoKust.java
@@ -1,5 +1,7 @@
 package br.com.devsenior.practice_kube_argo_kust.resources;
 
+import java.time.Instant;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,7 @@ public class PracticeKubeArgoKust {
 
     @GetMapping(value = "/health")
     public ResponseEntity<String> health() {
-        String health = "Status Ok";
+        String health = "Status Ok: " + Instant.now();
         return ResponseEntity.ok(health);
     }
     
